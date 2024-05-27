b30031a915a22990e0ea65ab06329bc7e1dee166
Lai Kok Wui <47911209+Laikokwui@users.noreply.github.com>
2024-05-27 11:32:09 +0800

    Update deleteEquipmentModal.tsx

--- frontend/src/components/Modal/deleteEquipmentModal.tsx
+++ frontend/src/components/Modal/deleteEquipmentModal.tsx
@@ -8,26 +8,34 @@ const DeleteConfirmationDialog = ({ open, data, onClose, onDelete }) => {
     };
 
     return (
-        <Dialog open={open} onClose={onClose} classes={{ paper: 'rounded-lg p-4' }}>
-            <DialogTitle className="text-2xl font-bold text-gray-800">Delete Confirmation</DialogTitle>
-            <DialogContent className="text-gray-600">
-                Are you sure you want to delete <span className="font-semibold">{data}</span>?
-            </DialogContent>
-            <DialogActions className="flex justify-end p-4">
-                <Button 
-                    onClick={onClose} 
-                    className="px-4 py-2 rounded-md mr-2 text-gray-600 bg-transparent hover:bg-gray-100 transition-colors duration-200"
-                >
-                    Cancel
-                </Button>
-                <Button 
-                    onClick={handleDelete} 
-                    className="px-4 py-2 rounded-md text-white bg-red-600 hover:bg-red-800 transition-colors duration-200"
-                >
-                    Delete
-                </Button>
-            </DialogActions>
-        </Dialog>
+        <div className="mb-5">
+            <button 
+                onClick={() => handleOpenDialog('Item 1')} 
+                className="text-red-600 hover:text-red-800 transition-colors duration-200"
+            >
+                <DeleteIcon />
+            </button>
+            <Dialog open={open} onClose={onClose} classes={{ paper: 'rounded-lg p-4' }}>
+                <DialogTitle className="text-2xl font-bold text-gray-800">Delete Confirmation</DialogTitle>
+                <DialogContent className="text-gray-600">
+                    Are you sure you want to delete <span className="font-semibold">{data}</span>?
+                </DialogContent>
+                <DialogActions className="flex justify-end p-4">
+                    <Button 
+                        onClick={onClose} 
+                        className="px-4 py-2 rounded-md mr-2 text-gray-600 bg-transparent hover:bg-gray-100 transition-colors duration-200"
+                    >
+                        Cancel
+                    </Button>
+                    <Button 
+                        onClick={handleDelete} 
+                        className="px-4 py-2 rounded-md text-white bg-red-600 hover:bg-red-800 transition-colors duration-200"
+                    >
+                        Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
     );
 };
 
